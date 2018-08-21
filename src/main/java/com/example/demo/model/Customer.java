d5a6fcb5b08d3507bfc80e256acc92cbc3fa4bba
staporekmarta <staporekmarta1@gmail.com>
2018-08-21 10:32:10 +0200

    Implement customer class

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,4 +1,24 @@
 package com.example.demo.model;
 
-public class Customer {
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    public Customer() {
+    }
+
+
 }
