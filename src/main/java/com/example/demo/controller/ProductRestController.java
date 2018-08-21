e3013fe8cac593e5172bb28ee579df6758b65c58
filipHartman <filip.hartman23@gmail.com>
2018-08-21 23:13:39 +0200

    Add creating new product by post method

--- src/main/java/com/example/demo/controller/ProductRestController.java
+++ src/main/java/com/example/demo/controller/ProductRestController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Product;
 import com.example.demo.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +19,12 @@ public class ProductRestController {
     @Autowired
     public ProductRestController(Service productService) {
         this.productService = productService;
+    }
+
+    @PostMapping(value = "/products")
+    public ResponseEntity createProduct(@RequestBody Product product) {
+        productService.save(product);
+
+        return new ResponseEntity(product, HttpStatus.OK);
     }
 }
