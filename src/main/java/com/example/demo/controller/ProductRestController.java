26663ddc04080197c9bd31067da86353d2f14615
filipHartman <filip.hartman23@gmail.com>
2018-08-21 23:31:25 +0200

    Add getting all products

--- src/main/java/com/example/demo/controller/ProductRestController.java
+++ src/main/java/com/example/demo/controller/ProductRestController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class ProductRestController {
@@ -20,6 +23,12 @@ public class ProductRestController {
     public ProductRestController(Service productService) {
         this.productService = productService;
     }
+
+    @GetMapping(value = "/products", produces = "application/json")
+    public Collection<Product> getAllProducts() {
+        return productService.findAll();
+    }
+
 
     @PostMapping(value = "/products")
     public ResponseEntity createProduct(@RequestBody Product product) {
