06378da6cded6375914f267558df18cfc000fa1f
filipHartman <filip.hartman23@gmail.com>
2018-08-21 11:43:23 +0200

    Change class to enum type

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,24 +1,9 @@
 package com.example.demo.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "categories")
-@Getter
-@Setter
-
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
-
-    @Column(name = "category_name")
-    private String name;
-
-    public Category() {
-    }
+public enum Category {
+    CLOTHES,
+    DRINK,
+    FOOD,
+    ELECTRONICS,
+    FURNITURE
 }
