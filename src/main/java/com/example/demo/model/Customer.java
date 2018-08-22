31d9f026f08755dff357c315a08cc1fd7cfc8db3
filipHartman <filip.hartman23@gmail.com>
2018-08-22 18:06:46 +0200

    Change name of table and column names of relation table between customers and baskets

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -14,11 +14,17 @@ public class Customer implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="customer_id")
     private Long id;
 
     private String name;
 
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(
+            name = "baskets_of_customers",
+            joinColumns = @JoinColumn(name = "customer_id"),
+            inverseJoinColumns = @JoinColumn(name = "basket_id")
+    )
     private List<Basket> baskets;
 
     public Customer() {
