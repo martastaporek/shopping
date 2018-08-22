d3bbe635f857ba31fb437d22bb24000c5ebb94ea
filipHartman <filip.hartman23@gmail.com>
2018-08-22 14:28:51 +0200

    Add method to get all baskets from database

--- src/main/java/com/example/demo/controller/BasketRestController.java
+++ src/main/java/com/example/demo/controller/BasketRestController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Basket;
 import com.example.demo.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class BasketRestController {
@@ -14,5 +18,10 @@ public class BasketRestController {
     @Autowired
     public BasketRestController(Service basketService) {
         this.basketService = basketService;
+    }
+
+    @GetMapping(value = "/baskets", produces = "application/json")
+    public Collection<Basket> getAllBaskets() {
+        return basketService.findAll();
     }
 }
