1f7655a38633e1d67f057c8f7d4ff70d8ebbd5e0
filipHartman <filip.hartman23@gmail.com>
2018-08-22 14:36:20 +0200

    Add getting basket by id

--- src/main/java/com/example/demo/controller/BasketRestController.java
+++ src/main/java/com/example/demo/controller/BasketRestController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Basket;
 import com.example.demo.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 public class BasketRestController {
@@ -23,5 +26,14 @@ public class BasketRestController {
     @GetMapping(value = "/baskets", produces = "application/json")
     public Collection<Basket> getAllBaskets() {
         return basketService.findAll();
+    }
+
+    @GetMapping(value = "/baskets/{basketid}", produces = "application/json")
+    public Basket getBasketById(@PathVariable(value = "basketId") Long basketId) {
+        Optional<Basket> basket = basketService.findById(basketId);
+        if(!basket.isPresent()) {
+            throw new ResourceNotFoundException("Basket", "id", basketId);
+        }
+        return basket.get();
     }
 }
